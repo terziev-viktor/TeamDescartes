4959929f12fd2715ae3dff6a613546da6a54d817
kitsune0408 <kitsune0408@yahoo.com>
2016-01-17 12:27:56 +0200

    reworked the code so that the background is moving using JavaScript now

---
+++ js/background.js
@@ -0,0 +1,63 @@
+(function() {
+	window.requestAnimationFrame = window.requestAnimationFrame
+			|| window.webkitRequestAnimationFrame
+			|| window.mozRequestAnimationFrame || function(callback) {
+				window.setTimeout(callback, 1000 / 60);
+			};
+
+	var canvas = document.getElementById('gamefield');
+	var context = canvas.getContext('2d');
+	var looping = false;
+	var totalSeconds = 0;
+
+	var img = new Image();
+	img.onload = imageLoaded;
+	img.src = 'img/background.png';
+
+	function imageLoaded() {
+		draw(0);
+
+		var btn = document.getElementById('start-button');
+		btn.addEventListener('click', function() {
+			startGame();
+		});
+	}
+
+	var lastFrameTime = 0;
+
+	function startGame() {
+		looping = true;		
+			lastFrameTime = Date.now();
+			requestAnimationFrame(loop);
+	}
+
+	function loop() {
+		if (!looping) {
+			return;
+		}
+
+		requestAnimationFrame(loop);
+
+		var now = Date.now();
+		var deltaSeconds = (now - lastFrameTime) / 1000;
+		lastFrameTime = now;
+		draw(deltaSeconds);
+	}
+
+	function draw(delta) {
+		totalSeconds += delta;
+
+		var speed = 100; // the background scrolls with a speed of 100
+							// pixels/sec
+		var numImages = Math.ceil(canvas.width / img.width) + 1;
+		var x_pos = totalSeconds * speed % img.width;
+
+		context.save();
+		context.translate(-x_pos, 0);
+		for (var i = 0; i < numImages; i++) {
+			context.drawImage(img, i * img.width, 0);
+		}
+		context.restore();
+	}
+}());
+
