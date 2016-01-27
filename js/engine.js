669bbd00314b524a93c2883b5d35855eb7b79368
Viktor Terziev <Viktor Terziev>
2016-01-27 21:21:59 +0200

    Added requestAnimationFrame for different browsers

--- js/engine.js
+++ js/engine.js
@@ -5,6 +5,11 @@
 var app = app || {};
 
 app.engine = (function () {
+    window.requestAnimationFrame = window.requestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.mozRequestAnimationFrame || function(callback) {
+        window.setTimeout(callback, 1000 / 60);
+    };
     function Engine() {
 
         this.gameIsRunning = false;
